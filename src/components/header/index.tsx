4206e0a73f372b3b0df883c98fa027866d39fd41
Bruna Santos <brunahta2025@hotmail.com>
2023-01-10 18:08:38 -0300

    mobile-header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import "./styles.scss"
+import icon from '../../../public/icon.svg'
+import { List } from "phosphor-react"
 
 export function Header(){
 
     
     return(
-        <div>
-
-        </div>
+        <header>
+            <img src={icon} alt="icon" />
+            <h2>Bruna Santos</h2>
+            <List size={32} color="#fffafa" />
+         
+        </header>
     )
 }
