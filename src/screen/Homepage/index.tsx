ba333a175eae822a11b4dd7f0c4dd07d27f3fdca
Bruna Santos <brunahta2025@hotmail.com>
2023-01-10 18:09:09 -0300

    linear-gradient

--- src/screen/Homepage/index.tsx
+++ src/screen/Homepage/index.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+import "./styles.scss"
 
 export function HomePage(){
 
     
     return(
-        <div>
-            <h1>Meu portfolio</h1>
+        <div className="HomePage-container">
+            <div className="img-wrapper">
+            <img src="https://picsum.photos/200" alt="" />
+            </div>
+
+
+            <div className="text">
+            <h2>
+                Introdução
+            </h2>
+            <p className="top-text"> I enjoy build websites. </p>
+             <p>Todos os dias é dia de <strong>aprender</strong>.</p> 
+            </div>
+    
         </div>
     )
 }
