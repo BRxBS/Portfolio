59ff1c4fee31465a1879aa86e5856b14e6388923
Bruna Santos <brunahta2025@hotmail.com>
2023-01-11 18:44:28 -0300

    newPages

---
+++ src/screen/AboutPage/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss"
+
+export function AboutPage(){
+
+    
+    return(
+        <div className="about-container">
+
+    
+        </div>
+    )
+}
---
+++ src/screen/ProjectPage/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.scss"
+
+export function projectsPage(){
+
+    
+    return(
+        <div className="projectsPage-container">
+
+    
+        </div>
+    )
+}
