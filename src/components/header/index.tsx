97b0a4a75ca4b53115208248d98a7f81856945c2
Bruna Santos <brunahta2025@hotmail.com>
2023-01-11 18:46:30 -0300

    media only

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import "./styles.scss"
 import icon from '../../../public/icon.svg'
-import { List } from "phosphor-react"
+import { EnvelopeSimple, List, } from "phosphor-react"
 
 export function Header(){
 
@@ -9,7 +9,18 @@ export function Header(){
         <header>
             <img src={icon} alt="icon" />
             <h2>Bruna Santos</h2>
-            <List size={32} color="#fffafa" />
+            <List size={32} color="#fffafa" className="svgList" />
+
+            <ul>
+                <li> Home</li>
+                <li> About</li>
+                <li> Projects</li>
+            </ul>
+
+            <div className="contact-wrapper">
+            <EnvelopeSimple size={32} color="#fffafa"  className="svgEnvelope" />
+            <p>Talk to me.</p>
+            </div>
          
         </header>
     )
