e8f78ce44326af3019321ceaf6c6d2a8bc9eb1b6
Bruna Santos <brunahta2025@hotmail.com>
2023-01-12 18:09:11 -0300

    routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./screen/Homepage";
+import { AboutPage } from "./screen/AboutPage";
+import { ProjectsPage } from "./screen/ProjectPage";
+import { SideBar } from "./components/sideBar";
 
 
 
@@ -10,6 +13,8 @@ export function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/projects " element={<ProjectsPage />} />
     </Routes>
   );
 }
