c4951ce014181e3706168b358f0969cbef34b9e7
Bruna Santos <brunahta2025@hotmail.com>
2023-01-12 18:11:11 -0300

    isOpen

--- src/components/sideBar/index.tsx
+++ src/components/sideBar/index.tsx
@@ -1,7 +1,49 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom"
+import {  List, X } from "phosphor-react"
+import "./styles.scss"
 
-export function SiderBar(){
+
+
+export function SideBar(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handler = () => setIsOpen(false)
+
+        window.addEventListener('click', handler)
+        return () => {
+            window.addEventListener('click', handler)
+        }
+    },[])
+    const handleInputClick = (e: { stopPropagation: () => void; }) => {
+        e.stopPropagation()
+        setIsOpen(!isOpen)
+    }
     return(
-        <>
-        </>
+
+        <div className="dropdown_cart_container">
+        <div className="container-aside" onClick={handleInputClick}>
+            {isOpen ? 
+          
+             <X size={32} 
+                color="#fffafa" 
+                className="svgX" />      
+            : 
+             <List size={32} 
+                   color="#fffafa" 
+                   className="svgList" />}
+         { isOpen  &&   (          
+                <aside>
+                <ul>
+                    <li> <Link to={`/`}>Home</Link> </li>
+                    <li> <Link to={`/aboutPage`}> About</Link></li>
+                    <li> <Link to={`/projects`}>Projects</Link> </li>
+                </ul>
+                </aside> )}
+
+        </div>
+        </div>
+
     )
 }
