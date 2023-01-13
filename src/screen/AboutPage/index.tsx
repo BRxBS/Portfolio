69d1ef8cfce7dcebe50c855d56e8f232aa9b18ce
Bruna Santos <brunahta2025@hotmail.com>
2023-01-13 15:21:36 -0300

    AboutPage testing

--- src/screen/AboutPage/index.tsx
+++ src/screen/AboutPage/index.tsx
@@ -5,7 +5,17 @@ export function AboutPage(){
     
     return(
         <div className="about-container">
-
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Corrupti perspiciatis excepturi, cumque laudantium laboriosam
+              necessitatibus molestias 
+             architecto temporibus deleniti illum 
+            cum fugiat nulla alias. Nulla obcaecati eaque laboriosam fugiat enim?
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+            Velit minus magni, sunt sit unde explicabo corporis ipsam fugiat 
+            laboriosam, ut, consequatur amet natus? Eum beatae debitis tempore 
+            ex quis? Natus?
+            </p> 
+            <iframe src="https://github.com/BRxBS" frameBorder="0"></iframe>
     
         </div>
     )
