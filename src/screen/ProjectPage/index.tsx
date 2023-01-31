84719bf4b22a85c434c3ecace17083ad3a59f1f2
Bruna Santos <brunahta2025@hotmail.com>
2023-01-31 19:25:00 -0300

    graphql

--- src/screen/ProjectPage/index.tsx
+++ src/screen/ProjectPage/index.tsx
@@ -2,41 +2,51 @@ import {gql, useQuery} from '@apollo/client'
 import { useState } from "react"
 import "./styles.scss"
 
+
 const GET_INFO_QUERY = gql`
-query {
-    portfolio {
-      id
-      iframeUrl
-      title
-      discription
-    }
+query info {
+  portfolios {
+    id
+    title
+    iframeUrl
+    description
   }
+}
 
 `
 interface GetInfoQueryResponse 
-{portfolio: {
-  id:string
+{portfolios: {
+  id: string
   iframeUrl: string
   title: string
-  discription: string
+  description: string
 }[]}
 
 export function ProjectsPage(){
     const { data } = useQuery<GetInfoQueryResponse>(GET_INFO_QUERY);
-// const [readMore, setMore] = useState(false)
+    const [expandedMap, setExpandedMap] = useState<{ [key: string]: boolean }>({});
+   
+    const toggleExpand = (newInfo : any) => {
+      setExpandedMap(prevExpandedMap => ({
+        ...prevExpandedMap,
+        [newInfo.id]: !prevExpandedMap[newInfo.id]
+      }));
+    };
+
     
     return(
         <div className="projectsPage-container">
             
-                {data?.portfolio.map((info) => {
+                {data?.portfolios.map((info) => {
+                     const newInfo = {...info};
                     return(
-                        <div className="ted-ed" key=
-                        {info.id}>
-                        <h2 className="ted-ed-h2">{info.title}</h2>
+                        <div className="info-content" key={info.id} >
+                        <h2 className="info-title">{info.title}</h2>
                         <iframe src={info.iframeUrl}
                          frameBorder="0"></iframe>
 
-                        <p>{info.discription}</p>
+                        <p className={`info-discription desc-text ${expandedMap[newInfo.id]  ? "expanded" : ""}`}>{info.description}</p>
+                        <p className="read-more-btn"  onClick={() => toggleExpand(newInfo)}>{expandedMap[newInfo.id]  ? 'Read Less' : 'Read More'}</p>
                         </div>
                     )
                 })}
