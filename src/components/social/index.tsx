09de078dcc2e5fb6402291a5cdfc2d545e91dddf
Bruna Santos <brunahta2025@hotmail.com>
2023-02-07 18:00:27 -0300

    social media

---
+++ src/components/social/index.tsx
@@ -0,0 +1,33 @@
+import "./styles.scss"
+
+export function Social(){
+
+    return(
+        <main className="main-social">
+            <div>
+                <p>
+                Há anos escolhi não estar presente nas redes sociais.
+                </p>
+                <p>
+                Atualmente número de usuários ativos do Facebook e Instagram somam 4,239 bilhões, 
+                eu escolhi não mais participar desse sistema que considero ser paralelo à realidade,
+                tomei essa decisão do ano de 2019, pois me sentia escrava das redes sócias, 
+                “Existem apenas duas indústrias que chamam seus clientes de usuários: a de drogas 
+                e a de software” nas palavras do professor de estatística da Universidade de Yale, Edward Tufte.
+                </p>
+                <p>
+                Hoje em dia faço o uso somente do WhatsApp e do LinkedIn, plataforma que vos falo. 
+                Sou uma profissional seria, e estou a procura de trabalho, sou desenvolvedora de React 
+                e para mostrar o meu desempenho estou no GitHub sempre atualizando os meus projetos 
+                e deixando você empregador, saber que tenho projetos reais e dedicação constante.
+                </p>
+ 
+ 
+
+
+            </div>
+ 
+        </main>
+
+    )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./screen/Homepage";
 import { AboutPage } from "./screen/AboutPage";
 import { ProjectsPage } from "./screen/ProjectPage";
+import { Social } from "./components/social"
 
 
 
@@ -14,6 +15,7 @@ export function Router() {
       <Route path="/" element={<HomePage />} />
       <Route path="/aboutPage" element={<AboutPage />} />
       <Route path="/projectsPage" element={<ProjectsPage />} />
+      <Route path="/social" element={<Social />} />
     </Routes>
   );
 }
