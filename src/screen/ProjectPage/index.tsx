8dcde32dea4c7882f053b054a26d3fc174359e35
Bruna Santos <brunahta2025@hotmail.com>
2023-06-06 18:19:57 -0300

    loading and padding

--- src/screen/ProjectPage/index.tsx
+++ src/screen/ProjectPage/index.tsx
@@ -1,78 +1,114 @@
-import {gql, useQuery} from '@apollo/client'
-import { useState } from "react"
-import ReactPlayer from 'react-player'
-import "./styles.scss"
-
+import { gql, useQuery } from "@apollo/client";
+import { useState } from "react";
+import ReactPlayer from "react-player";
+import "./styles.scss";
 
 const GET_INFO_QUERY = gql`
-query info {
-  portfolios {
-    id
-    title
-    iframeUrl
-    description
-    repoUrl
-    videoUrl
-  }
+    query info {
+        portfolios {
+            id
+            title
+            iframeUrl
+            description
+            repoUrl
+            videoUrl
+        }
+    }
+`;
+interface GetInfoQueryResponse {
+    portfolios: {
+        id: string;
+        iframeUrl: string;
+        title: string;
+        description: string;
+        repoUrl: string;
+        videoUrl: string;
+    }[];
 }
 
-`
-interface GetInfoQueryResponse 
-{portfolios: {
-  id: string
-  iframeUrl: string
-  title: string
-  description: string
-  repoUrl:string
-  videoUrl:string
-}[]}
+export function ProjectsPage() {
+    const { data, loading } = useQuery<GetInfoQueryResponse>(GET_INFO_QUERY);
+    const [expandedMap, setExpandedMap] = useState<{ [key: string]: boolean }>(
+        {}
+    );
 
-export function ProjectsPage(){
-    const { data } = useQuery<GetInfoQueryResponse>(GET_INFO_QUERY);
-    const [expandedMap, setExpandedMap] = useState<{ [key: string]: boolean }>({});
-   
-    
-    const toggleExpand = (newInfo : any) => {
-      setExpandedMap(prevExpandedMap => ({
-        ...prevExpandedMap,
-        [newInfo.id]: !prevExpandedMap[newInfo.id]
-      }));
+    const toggleExpand = (newInfo: any) => {
+        setExpandedMap((prevExpandedMap) => ({
+            ...prevExpandedMap,
+            [newInfo.id]: !prevExpandedMap[newInfo.id],
+        }));
     };
 
-    
-    return(
-      <>
-        <div className="projectsPage-container">
-            
-                {data?.portfolios.map((info) => {
-                     const newInfo = {...info};
-                    return(
-                      <div className='info-wrapper' key={info.id}  >
-                        <div className="info-content" >
-                        <h2 className="info-title">{info.title}</h2>
-                        
-                       
-                       {info.iframeUrl ? <iframe src={info.iframeUrl}
-                         frameBorder="0"></iframe> : 
-                         <ReactPlayer url={info.videoUrl}  width='100%'
-                         height='100%' />}
+    return (
+        <>
+            {loading ? (
+                <div>loading...</div>
+            ) : (
+                <div className="projectsPage-container">
+                    {data?.portfolios.map((info) => {
+                        const newInfo = { ...info };
+                        return (
+                            <div className="info-wrapper" key={info.id}>
+                                <div className="info-content">
+                                    <h2 className="info-title">{info.title}</h2>
 
-                        {info.iframeUrl ? <a href={info.iframeUrl} 
-                                            target='blanck'  
-                                            className='a-iframeUrl'> Ir para Página.</a> 
-                                        :  ''}
+                                    {info.iframeUrl ? (
+                                        <iframe
+                                            src={info.iframeUrl}
+                                            frameBorder="0"
+                                        ></iframe>
+                                    ) : (
+                                        <ReactPlayer
+                                            url={info.videoUrl}
+                                            width="100%"
+                                            height="100%"
+                                        />
+                                    )}
 
-                        <p className={`info-discription desc-text ${expandedMap[newInfo.id]  ? "expanded" : ""}`}>{info.description}</p>
-                        <p className="read-more-btn"  onClick={() => toggleExpand(newInfo)}>{expandedMap[newInfo.id]  ? 'Read Less' : 'Read More'}</p>
-                        
-                        <a href={info.repoUrl} target='blanck' className='repoUrl'>Ir ao Repositorio.</a>
-                        
-                        </div>
-                        </div>
-                    )
-                })}
-        </div>
-    
-</>
-    )
+                                    {info.iframeUrl ? (
+                                        <a
+                                            href={info.iframeUrl}
+                                            target="blanck"
+                                            className="a-iframeUrl"
+                                        >
+                                            {" "}
+                                            Ir para Página.
+                                        </a>
+                                    ) : (
+                                        ""
+                                    )}
+
+                                    <p
+                                        className={`info-discription desc-text ${
+                                            expandedMap[newInfo.id]
+                                                ? "expanded"
+                                                : ""
+                                        }`}
+                                    >
+                                        {info.description}
+                                    </p>
+                                    <p
+                                        className="read-more-btn"
+                                        onClick={() => toggleExpand(newInfo)}
+                                    >
+                                        {expandedMap[newInfo.id]
+                                            ? "Read Less"
+                                            : "Read More"}
+                                    </p>
+
+                                    <a
+                                        href={info.repoUrl}
+                                        target="blanck"
+                                        className="repoUrl"
+                                    >
+                                        Ir ao Repositorio.
+                                    </a>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
+        </>
+    );
 }
